fd6a25686a27bac05d36291f9ad4325ece442fbf
Nicolasargenton456 <ds.46276553808@gmail.com>
2024-04-18 11:06:29 -0300

    first commit

---
+++ src/screens/Home.js
@@ -0,0 +1,14 @@
+import { View, Text, StyleSheet} from "react-native";
+
+export default function Home({route}) {
+  const {username} = route.params
+  return (
+    <View >
+      <Text> </Text>
+      <Text></Text>
+    </View>
+  )};
+
+const styles = StyleSheet.create({
+        backgraudcolor: "#640C0C",
+    });
